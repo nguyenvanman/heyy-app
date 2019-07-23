906dbd1eaa949b15edf6593e7ad43a6deb4eb4da
Man Nguyen <nguyenvanman>
2019-07-23 14:44:24 +0700

    Fixed cors issue

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,17 +6,21 @@ class AuthenticationController < ApplicationController
 
     # POST /authenticate
     def authenticate
-        Net::HTTP.start(info_uri.host, info_uri.port, :use_ssl => true) do |http|
-            request = Net::HTTP::Get.new info_uri
-            response = http.request(request)
-            user_params = JSON.parse response.body
-            user = User.find_or_create_by(name: user_params['name'], email: user_params['email'], uid: user_params['id'])
-            if user.valid? 
-                render json: { message: "Success", data: user }, status: response.code
-            else
-                render json: { message: "Failed", error: JSON.parse(response.body)['error'] }, status: response.code
-            end
-        end  
+        begin
+            Net::HTTP.start(info_uri.host, info_uri.port, :use_ssl => true) do |http|
+                request = Net::HTTP::Get.new info_uri
+                response = http.request(request)
+                user_params = JSON.parse response.body
+                user = User.find_or_create_by(name: user_params['name'], email: user_params['email'], uid: user_params['id'])
+                if user.valid? 
+                    render json: { message: "Success", user: user }, status: response.code
+                else
+                    render json: { message: "Failed", error: JSON.parse(response.body)['error'] }, status: response.code
+                end
+            end  
+        rescue => exception
+            render json: { message: "Failed", error: exception.message }, status: :bad_request
+        end
     end
 
     def info_uri
