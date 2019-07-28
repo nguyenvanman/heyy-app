58220eef5584be92dd55f567c5b533311c97f58d
Man Nguyen <nguyenvanman>
2019-07-28 19:53:59 +0700

    Added image url to authenticate response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-
 require 'json'
 
 class AuthenticationController < ApplicationController
@@ -10,17 +9,15 @@ class AuthenticationController < ApplicationController
             Net::HTTP.start(info_uri.host, info_uri.port, :use_ssl => true) do |http|
                 request = Net::HTTP::Get.new info_uri
                 response = http.request(request)
-                user_params = JSON.parse response.body
-                user = User.find_or_create_by(name: user_params['name'].to_s, email: user_params['email'].to_s, uid: user_params['id'].to_s, password_digest: "password")
-                if user.valid? 
-                    token = JsonWebToken.encode(id: user.id)
-                    exp = Time.now + 72.hours.to_i
+                user_params = user_params(response)
+                user = User.find_or_create_by(uid: user_params[:uid], name: user_params[:name])
+                if user.update_attributes(user_params) && user.valid? 
                     render json: { 
                         message: "Success", 
                         user: UserSerializer.new(user) ,
-                        access_token: token,
-                        expired_time: exp
-                    }, status: response.code
+                        access_token: JsonWebToken.encode(id: user.id),
+                        expired_time: Time.now + 72.hours.to_i
+                    }, status: :ok
                 else
                     render json: { message: "Failed", error: JSON.parse(response.body)['error'] }, status: :bad_request
                 end
@@ -29,6 +26,17 @@ class AuthenticationController < ApplicationController
             render json: { message: "Failed", error: exception.message }, status: :bad_request
         end
     end
+
+    def user_params(response)
+        user_params = Hash.new
+        response_params = JSON.parse response.body
+        user_params[:uid] = response_params['id'].to_s
+        user_params[:name] = response_params['name'].to_s
+        user_params[:email] = response_params['email'].to_s
+        user_params[:image_url] = "https://graph.facebook.com/#{response_params['id']}/picture?type=large"
+        user_params[:password_digest] = 'password'
+        return user_params
+    end 
 
     def info_uri
         base_uri = 'https://graph.facebook.com/me'
