5bb4a7d2276248afb20aaeee3c4f40f191c908c7
nguyenvanman <vanman.lqd@gmail.com>
2019-10-16 09:44:45 +0700

    allow null answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   end
   
   def question_params
-    params.require([:question, :answer])
+    params.require([:question])
     params.permit(:question, :answer)
   end
 end
